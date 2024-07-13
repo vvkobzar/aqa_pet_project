1fd660aa370b17a36d0f9717d347e4452485ff87
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-07-13 14:33:50 +0300

    changed in base_page page opening

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,12 +3,11 @@ from selenium.webdriver.support.ui import WebDriverWait as wait
 
 
 class BasePage:
-    def __init__(self, driver, url):
+    def __init__(self, driver):
         self.driver = driver
-        self.url = url
 
     def open(self):
-        self.driver.get(self.url)
+        self.driver.get(self.PAGE_URL)
 
     def element_is_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
