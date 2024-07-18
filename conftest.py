49ed02b0b9914371e9ad680807baf0e6e5fee013
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-07-18 12:04:50 +0300

    added conftest: - options - the directory for downloading files

--- conftest.py
+++ conftest.py
@@ -1,14 +1,22 @@
+import os
+
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
+preferences = {
+    "download.default_directory": os.path.join(os.getcwd(), "downloads")
+}
+options = Options()
+options.add_experimental_option("prefs", preferences)
 service = Service(ChromeDriverManager().install())
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
