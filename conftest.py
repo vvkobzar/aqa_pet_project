b9e631ac2356a80fd0ffb72bfc081f19fbc550d7
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-07-20 12:38:05 +0300

    added options for ci: - options.add_argument("--no-sandbox") - options.add_argument("--disable-dev-shm-usage") - options.add_argument("--window-size=1920,1080")

--- conftest.py
+++ conftest.py
@@ -13,6 +13,9 @@ preferences = {
 options = Options()
 options.add_experimental_option("prefs", preferences)
 options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1920,1080")
 service = Service(ChromeDriverManager().install())
 
 
