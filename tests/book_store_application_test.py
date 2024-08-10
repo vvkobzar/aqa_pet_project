1d11d2acd20c76ae786645b7d0e6ea1f6eef12a0
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-08-10 20:10:04 +0300

    added error descriptions to checks

--- tests/book_store_application_test.py
+++ tests/book_store_application_test.py
@@ -20,12 +20,16 @@ class TestBookStoreApplication:
             self.username, self.password = login_page.fill_fields_register_form()
             success_alert = login_page.check_register_success()
 
-            assert success_alert == "User Register Successfully."
+            assert success_alert == "User Register Successfully.", (
+                "error during user registration"
+            )
 
             yield
 
             delite_alert = login_page.delete_user()
-            assert delite_alert == "User Deleted."
+            assert delite_alert == "User Deleted.", (
+                "failed to delete a user"
+            )
 
         def test_login_to_book_store(self, driver):
             login_page = LoginPage(driver)
@@ -34,5 +38,9 @@ class TestBookStoreApplication:
             login_page.login_to_book_store(self.username, self.password)
             url_page, profile_user_name_text = login_page.check_login_success()
 
-            assert url_page == "https://demoqa.com/profile"
-            assert self.username == profile_user_name_text
+            assert url_page == "https://demoqa.com/profile", (
+                "the profile page didn't open"
+            )
+            assert self.username == profile_user_name_text, (
+                "user name does not match the name in the profile"
+            )
