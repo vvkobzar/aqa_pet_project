82141faf58fd21a0482392774b2dac5e9fd2a3f8
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-08-16 12:17:45 +0300

    added test_added_books_in_the_profile

--- tests/api_tests/book_store_test.py
+++ tests/api_tests/book_store_test.py
@@ -1,5 +1,6 @@
 import random
 from base.base_api import BaseAPI
+import pytest
 
 
 class TestBookStore(BaseAPI):
@@ -13,3 +14,16 @@ class TestBookStore(BaseAPI):
 
         assert random_book_from_list == book_in_the_profile, "the book has not been added to the profile"
         assert response_book == book_in_the_profile, "the books in the profile don't match the books in the response"
+
+    @pytest.mark.xfail(
+        reason="books from get_user query do not match the added books. In the ui profile the books match"
+    )
+    def test_added_books_in_the_profile(self, account_setup):
+        user_id, token = account_setup
+
+        isbn_books = set(self.api_book_store.get_isbn_books_list())
+        for book in isbn_books:
+            self.api_book_store.added_books_in_the_profile(book, user_id, token)
+        books_in_the_profile = set(self.api_account.get_user(user_id, token))
+
+        assert isbn_books == books_in_the_profile, "added books do not match the books in the profile"
