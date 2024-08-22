3cd6436916649d999d3a3f00ef5bb447806bdee5
Vladislav Kobzar <kobzarvllv@gmail.com>
2024-08-22 16:09:42 +0300

    add allure-report to book store application page

--- tests/ui_tests/book_store_application_test.py
+++ tests/ui_tests/book_store_application_test.py
@@ -1,16 +1,24 @@
-import pytest
 import os
+import allure
+import pytest
+
 from dotenv import load_dotenv
 from pages.book_store_application_page import LoginPage
 
 load_dotenv()
 
 
+@allure.epic("UI Tests")
+@allure.feature("Book Store Application")
 class TestBookStoreApplication:
+
+    @allure.story("Login")
     class TestLoginPage:
+
         username = os.getenv('USERNAME')
         password = os.getenv('PASSWORD')
 
+        @allure.title("Register to book store")
         @pytest.mark.xfail(reason="It's not going through because of the captcha.")
         # @pytest.fixture(scope='function', autouse=True)
         def register_to_book_store(self, driver):
@@ -20,17 +28,20 @@ class TestBookStoreApplication:
             self.username, self.password = login_page.fill_fields_register_form()
             success_alert = login_page.check_register_success()
 
-            assert success_alert == "User Register Successfully.", (
-                "error during user registration"
-            )
+            with allure.step("Check registration success"):
+                assert success_alert == "User Register Successfully.", (
+                    "error during user registration"
+                )
 
             yield
 
-            delite_alert = login_page.delete_user()
-            assert delite_alert == "User Deleted.", (
-                "failed to delete a user"
-            )
+            with allure.step("Delete user"):
+                delite_alert = login_page.delete_user()
+                assert delite_alert == "User Deleted.", (
+                    "failed to delete a user"
+                )
 
+        @allure.title("Login to book store")
         def test_login_to_book_store(self, driver):
             login_page = LoginPage(driver)
             login_page.open()
@@ -38,9 +49,11 @@ class TestBookStoreApplication:
             login_page.login_to_book_store(self.username, self.password)
             url_page, profile_user_name_text = login_page.check_login_success()
 
-            assert url_page == "https://demoqa.com/profile", (
-                "the profile page didn't open"
-            )
-            assert self.username == profile_user_name_text, (
-                "user name does not match the name in the profile"
-            )
+            with allure.step(f"Assert URL is correct: {url_page}"):
+                assert url_page == "https://demoqa.com/profile", (
+                    "the profile page didn't open"
+                )
+            with allure.step(f"Assert profile user name matches: {profile_user_name_text}"):
+                assert self.username == profile_user_name_text, (
+                    "user name does not match the name in the profile"
+                )
